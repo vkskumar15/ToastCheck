6aa8b49f723e80c660349ae15a6cefdbc4227152
vikas <vikas.dfavo@gmail.com>
2024-02-15 11:14:04 +0530

    test

--- ToastCheck/build.gradle.kts
+++ ToastCheck/build.gradle.kts
@@ -55,3 +55,6 @@ afterEvaluate {
         }
     }
 }
+
+
+
--- app/src/main/java/com/services/toasty/MainActivity.java
+++ app/src/main/java/com/services/toasty/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.services.toastcheck.ToastCheck;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToastCheck.simpleToast(this,"vikas");
     }
 }
